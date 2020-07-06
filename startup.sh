6ad95f227c9ea911684388381c17634fd8042234
Alessandro Vaccarino <alessandro.vaccarino@gmail.com>
2020-07-06 10:08:47 +0200

    Updated startup.sh

--- startup.sh
+++ startup.sh
@@ -11,4 +11,5 @@ sleep 15
 docker exec -it mongosNode1 bash -c "echo 'sh.status()' | mongo "
 
 # Connection String:
-#mongo "mongodb://mongorshard1node1:27017,mongorshard1node2.local:27017,mongorshard1node3:27017/?replicaSet=mongorshard1"
+#mongo "mongodb://mongorshard1node1:27017,mongorshard1node2.local:27027,mongorshard1node3:27037/?replicaSet=mongorshard1"
+#mongo "mongodb://localhost:27017,localhost:27027,localhost:27037/?replicaSet=mongorshard1"
